57f64f437c56bcfa211e250f1030ead52580994e
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2022-07-26 03:34:52 +0700

    dựng base UI Navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,29 +1,58 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
+import { signOut } from "firebase/auth";
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/firebase.config";
+import { Button } from "../button";
+import { toast } from "react-toastify";
+
 const NavList = [
-    {
-        url: '/',
-        name:'HOME'
-    },
-    {
-        url: '/',
-        name:'BLOG'
-    },
-    {
-        url: '/',
-        name:'CONTACT'
-    },
-]
+  {
+    url: "/",
+    name: "HOME",
+  },
+  {
+    url: "/blog",
+    name: "BLOG",
+  },
+  {
+    url: "/contact",
+    name: "CONTACT",
+  },
+];
 
 const Header = () => {
-    return (
-        <div className='container-page'>
-            <a href="/" className='text-primary'>BLG</a>
-            <ul>
-                {/* <NavLink></NavLink> */}
-            </ul>
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    signOut(auth);
+    toast.success("Đăng xuất thành công", {
+      pauseOnHover: false,
+      autoClose: 2000,
+    });
+    setTimeout(() => {
+      navigate("/sign-in");
+    }, 2000);
+  }
+  return (
+    <div className="bg-white">
+      <div className="container-page flex justify-between items-center font-semibold">
+        <a href="/" className="text-primary text-4xl">
+          BLG
+        </a>
+        <div>
+          {/* <NavLink></NavLink> */}
+          {NavList.map((item) => (
+            <NavLink to={item.url} className={({ isActive }) => `px-6 ${isActive && 'navbar-active'}`}>
+              {item.name}
+            </NavLink>
+          ))}
         </div>
-    );
+        <Button type="button" onClick={handleSignOut}>
+          Sign Out
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,24 +7,11 @@ import { useNavigate } from "react-router-dom";
 import Header from "../components/layout/Header";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-
-  function handleSignOut() {
-    signOut(auth);
-    toast.success("Đăng xuất thành công", {
-      pauseOnHover: false,
-      autoClose: 2000,
-    });
-    setTimeout(() => {
-      navigate("/sign-in");
-    }, 2000);
-  }
+  
   return (
     <div>
       <Header></Header>
-      <Button type="button" onClick={handleSignOut}>
-        Sign Out
-      </Button>
+      
     </div>
   );
 };
